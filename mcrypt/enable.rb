78555321af2ebd34426ae6d18984302bb19c9e51
Peter Olds <polds@kyanicorp.com>
2014-08-15 19:43:13 -0600

    Try that

--- mcrypt/enable.rb
+++ mcrypt/enable.rb
@@ -4,12 +4,6 @@ node[:deploy].each do |application, deploy|
 		user "root"
 		code <<-EOH
 		/usr/sbin/php5enmod mcrypt
-		EOH
-	end
-	script "restart_apache2" do
-		interpreter "bash"
-		user "root"
-		code <<-EOH
 		/usr/sbin/service apache2 restart
 		EOH
 	end
