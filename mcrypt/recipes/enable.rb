2a8d2225aa297d33c477d111103f1273639495ed
Peter Olds <polds@kyanicorp.com>
2014-08-15 20:16:39 -0600

    Update enable.rb

--- mcrypt/recipes/enable.rb
+++ mcrypt/recipes/enable.rb
@@ -1,10 +1,8 @@
-node[:deploy].each do |application, deploy|
-	script "enable_mcrypt" do
-		interpreter "bash"
-		user "root"
-		code <<-EOH
-		/usr/sbin/php5enmod mcrypt
-		/usr/sbin/service apache2 restart
-		EOH
-	end
+script "enable_mcrypt" do
+	interpreter "bash"
+	user "root"
+	code <<-EOH
+	/usr/sbin/php5enmod mcrypt
+	/usr/sbin/service apache2 restart
+	EOH
 end
